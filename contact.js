4b66956e2161def94cf15d236d72e6f55015a246
AustinSkrobar <austin.skrobar@gmail.com>
2020-07-19 13:57:06 -0500

    update for contact and image gallery

---
+++ contact.js
@@ -0,0 +1,7 @@
+  
+const placeForResults = document.getElementById("contactResults")
+
+new URLSearchParams(window.location.search).forEach((value, name) => {
+  placeForResults.append(`${name} : ${value}`)
+  placeForResults.append(document.createElement("br"))
+})
